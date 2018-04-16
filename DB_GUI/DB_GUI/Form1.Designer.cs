d9ff8d6aecaf80fa0910582b972cf55797192c84
maniuuur <maniuuur@gmail.com>
2018-04-16 19:55:58 +0200

    DB_connect

--- DB_GUI/DB_GUI/Form1.Designer.cs
+++ DB_GUI/DB_GUI/Form1.Designer.cs
@@ -29,22 +29,88 @@
         private void InitializeComponent()
         {
             this.listView1 = new System.Windows.Forms.ListView();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.DelButton = new System.Windows.Forms.Button();
+            this.GetButton = new System.Windows.Forms.Button();
+            this.SortButton = new System.Windows.Forms.Button();
+            this.AddButton = new System.Windows.Forms.Button();
+            this.ConnectButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // listView1
             // 
-            this.listView1.Location = new System.Drawing.Point(249, 33);
+            this.listView1.Location = new System.Drawing.Point(221, 36);
             this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(291, 97);
+            this.listView1.Size = new System.Drawing.Size(140, 263);
             this.listView1.TabIndex = 0;
             this.listView1.UseCompatibleStateImageBehavior = false;
             this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(378, 36);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // DelButton
+            // 
+            this.DelButton.Location = new System.Drawing.Point(378, 163);
+            this.DelButton.Name = "DelButton";
+            this.DelButton.Size = new System.Drawing.Size(121, 33);
+            this.DelButton.TabIndex = 3;
+            this.DelButton.Text = "Del";
+            this.DelButton.UseVisualStyleBackColor = true;
+            // 
+            // GetButton
+            // 
+            this.GetButton.Location = new System.Drawing.Point(378, 202);
+            this.GetButton.Name = "GetButton";
+            this.GetButton.Size = new System.Drawing.Size(121, 32);
+            this.GetButton.TabIndex = 4;
+            this.GetButton.Text = "Get";
+            this.GetButton.UseVisualStyleBackColor = true;
+            // 
+            // SortButton
+            // 
+            this.SortButton.Location = new System.Drawing.Point(378, 240);
+            this.SortButton.Name = "SortButton";
+            this.SortButton.Size = new System.Drawing.Size(121, 33);
+            this.SortButton.TabIndex = 5;
+            this.SortButton.Text = "Sort";
+            this.SortButton.UseVisualStyleBackColor = true;
+            // 
+            // AddButton
+            // 
+            this.AddButton.Location = new System.Drawing.Point(378, 124);
+            this.AddButton.Name = "AddButton";
+            this.AddButton.Size = new System.Drawing.Size(121, 33);
+            this.AddButton.TabIndex = 6;
+            this.AddButton.Text = "Add";
+            this.AddButton.UseVisualStyleBackColor = true;
+            // 
+            // ConnectButton
+            // 
+            this.ConnectButton.Location = new System.Drawing.Point(378, 63);
+            this.ConnectButton.Name = "ConnectButton";
+            this.ConnectButton.Size = new System.Drawing.Size(121, 33);
+            this.ConnectButton.TabIndex = 7;
+            this.ConnectButton.Text = "Connect";
+            this.ConnectButton.UseVisualStyleBackColor = true;
+            this.ConnectButton.Click += new System.EventHandler(this.ConnectButton_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.ConnectButton);
+            this.Controls.Add(this.AddButton);
+            this.Controls.Add(this.SortButton);
+            this.Controls.Add(this.GetButton);
+            this.Controls.Add(this.DelButton);
+            this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.listView1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -55,6 +121,12 @@
         #endregion
 
         private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button DelButton;
+        private System.Windows.Forms.Button GetButton;
+        private System.Windows.Forms.Button SortButton;
+        private System.Windows.Forms.Button AddButton;
+        private System.Windows.Forms.Button ConnectButton;
     }
 }
 
--- DB_GUI/DB_GUI/Form1.cs
+++ DB_GUI/DB_GUI/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace DB_GUI
 {
@@ -14,19 +15,65 @@ namespace DB_GUI
     {
         public Form1()
         {
-            InitializeComponent();
+            InitializeComponent();    
 
-            ListViewItem lvi = new ListViewItem();
-            lvi.SubItems.Add("s");
-            lvi.SubItems.Add("");
-            lvi.SubItems.Add("dasd");
-
-            listView1.Items.Add(lvi);
+            listView1.View = View.Details;
+            listView1.FullRowSelect = true;
         }
 
         private void listView1_SelectedIndexChanged(object sender, EventArgs e)
         {
 
+        }
+        private void getDataFromDb()
+        {
+            string connectionString = "datasource=127.0.0.1;port=3306;username=root;password=;database=test";
+                                      
+
+            MySqlConnection databaseConnection = new MySqlConnection(connectionString);
+
+            try
+            {
+                databaseConnection.Open();
+
+                getDataIntoLv(databaseConnection);
+
+                databaseConnection.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private void getDataIntoLv(MySqlConnection databaseConnection)
+        {
+            string query = "SELECT * FROM users";
+            MySqlCommand commandDatabase = new MySqlCommand(query, databaseConnection);
+            commandDatabase.CommandTimeout = 60;
+            MySqlDataReader reader;
+            reader = commandDatabase.ExecuteReader();
+            listView1.Items.Clear();
+
+            if (reader.HasRows)
+            {
+                while (reader.Read())
+                {
+                    string[] row = { reader.GetInt32(0).ToString(), reader.GetString(1), reader.GetString(2) };
+                    var listViewItem = new ListViewItem(row);
+                    listView1.Items.Add(listViewItem);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Brak danych");
+            }
+            reader.Close();
+        }
+
+        private void ConnectButton_Click(object sender, EventArgs e)
+        {
+            getDataFromDb();
         }
     }
 }
