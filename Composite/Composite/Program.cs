9e48f80b032a15973a3fbd33dcb95493376c5a70
maniuuur <maniuuur@gmail.com>
2018-06-05 23:18:58 +0200

    example

---
+++ Composite/Composite/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Composite
+{
+    class MainApp
+    {
+
+        static void Main()
+        {
+            CompositeElement root = new CompositeElement("Obrazek");
+
+            root.Add(new PrimitiveElement("czerwona linia"));
+            root.Add(new PrimitiveElement("niebieski okrąg"));
+            root.Add(new PrimitiveElement("zielony box"));
+
+            CompositeElement comp = new CompositeElement("dwa okręgi");
+
+            comp.Add(new PrimitiveElement("czarny okrąg"));
+            comp.Add(new PrimitiveElement("biały okrąg"));
+            root.Add(comp);
+            root.Add(new PrimitiveElement("różowy box"));
+
+            PrimitiveElement pe = new PrimitiveElement("żółta linia");
+
+            root.Add(pe);
+            root.Remove(pe);
+            root.Display(1);
+
+            Console.ReadKey();
+        }
+    }
+}
