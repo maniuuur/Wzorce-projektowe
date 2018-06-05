b2673d39bb2d5b158f8cf7bfb681c7c1758312f4
maniuuur <maniuuur@gmail.com>
2018-06-05 23:32:11 +0200

    Proxy structural example
    Strukturalny kod wzorca Proxy

---
+++ Proxy/Proxy/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Proxy
+
+{
+    class MainApp
+    {
+        static void Main()
+        {
+            Proxy proxy = new Proxy();
+            proxy.Request();
+            Console.ReadKey();
+        }
+    }  
+}
+
+
---
+++ Proxy/Proxy/Proxy.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proxy
+{
+    class Proxy : Object
+    {
+        private RealObject _realObject;
+
+        public override void Request()
+        {
+            if (_realObject == null)
+            {
+                _realObject = new RealObject();
+            }
+
+            _realObject.Request();
+        }
+    }
+}
